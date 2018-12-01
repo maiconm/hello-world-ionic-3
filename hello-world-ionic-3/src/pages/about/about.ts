20828b224e1b8d016ef8650bce2ff029300a550e
Maicon Andraski <mandraski@new-host-2.home>
2018-12-01 17:47:14 -0200

    Dialog, cards, button, images and icons

--- hello-world-ionic-3/src/pages/about/about.ts
+++ hello-world-ionic-3/src/pages/about/about.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +7,41 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public alertCtrl: AlertController,
+    public navCtrl: NavController
+  ) {
 
+  }
+
+  protected showConfirm() {
+    const confirm = this.alertCtrl.create({
+      title: 'Use this lightsaber?',
+      message: 'Do you agree to use this lightsaber to do good across the intergalactic galaxy?',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            const disagreed = this.alertCtrl.create({
+              title: `You've disagreed!`,
+              message: `so it's mean that you don't agree to use light saber to do good across the intergalactic galaxy! :/`
+            });
+            disagreed.present();
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+            const agreed = this.alertCtrl.create({
+              title: `You've agreed!`,
+              message: `so it's mean that you agree to use this lightsaber to do good across the intergalactic galaxy!`
+            });
+            agreed.present();
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 
 }
